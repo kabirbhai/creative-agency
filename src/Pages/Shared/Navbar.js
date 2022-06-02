f7e4c5538b06522958aedb67d51080890ed09baa
kabir <makkobir79@gmail.com>
2022-06-02 22:57:22 +0600

    navbar modified

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "../../images/logos/logo.png";
 
 const Navbar = () => {
   return (
@@ -6,7 +7,7 @@ const Navbar = () => {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
-            Navbar
+            <img width={70} src={logo} alt="logo" />
           </a>
           <button
             className="navbar-toggler"
@@ -26,42 +27,20 @@ const Navbar = () => {
                   Home
                 </a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Link
+              <li className="nav-item ">
+                <a className="nav-link active" href="/">
+                  Our portfolio
                 </a>
               </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
+              <li className="nav-item ">
+                <a className="nav-link active" href="/">
+                  Our Team
                 </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
+              </li>
+              <li className="nav-item ">
+                <a className="nav-link active" href="/">
+                  Contact us
+                </a>
               </li>
             </ul>
           </div>
