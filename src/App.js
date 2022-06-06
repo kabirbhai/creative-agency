53dd0894b361d4f8bc96aa113aad9bf3aa408b79
kabir <makkobir79@gmail.com>
2022-06-06 11:42:23 +0600

    router setup completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home";
+import Footer from "./Pages/Shared/Footer";
 import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
   return (
     <section>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/ourTeam" element={<Home />} />
+      </Routes>
+      <Footer />
     </section>
   );
 }
